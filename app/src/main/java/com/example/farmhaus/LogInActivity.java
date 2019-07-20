ec538c2b16215ae5ff7d0145f807321d6ac77bd9
Yizhe Zhao <zyz4317@126.com>
2019-07-20 16:02:42 -0700

    what's look for

--- app/src/main/java/com/example/farmhaus/LogInActivity.java
+++ app/src/main/java/com/example/farmhaus/LogInActivity.java
@@ -2,7 +2,10 @@ package com.example.farmhaus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        Button button1= (Button) findViewById(R.id.skipToW);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LogInActivity.this,WhatLookFor.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/farmhaus/WhatLookFor.java
@@ -0,0 +1,14 @@
+package com.example.farmhaus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhatLookFor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_look_for);
+    }
+}
