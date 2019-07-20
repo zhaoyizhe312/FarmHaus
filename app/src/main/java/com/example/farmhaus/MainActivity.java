834fdce1f8dedcd4c0891102bbf07a7ecd0b5996
Yizhe Zhao <zyz4317@126.com>
2019-07-20 15:39:32 -0700

    log in page

--- app/src/main/java/com/example/farmhaus/MainActivity.java
+++ app/src/main/java/com/example/farmhaus/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.farmhaus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1= (Button) findViewById(R.id.logInBut);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LogInActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
